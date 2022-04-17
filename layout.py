c4d4fdccdae1481a9049bc095df288f6851c7cbd
Durgesh0108 <103890880+Durgesh0108@users.noreply.github.com>
2022-04-17 21:01:10 +0530

    Update layout.py

--- layout.py
+++ layout.py
@@ -16,23 +16,3 @@ class Layout(Turtle):
             self.forward(540)
             self.right(90)
         self.end_fill()
-#
-# screen = Screen()
-# screen.bgcolor("black")
-# screen.setup(width=600, height=600)
-
-# def layout():
-#     tim = Turtle()
-#     tim.color("white")
-#     tim.penup()
-#     tim.goto(-280, 280)
-#     tim.color("white")
-#     tim.pendown()
-#     tim.hideturtle()
-#     for x in range(4):
-#         tim.forward(300)
-#         tim.right(90)
-
-
-# layout()
-# screen.exitonclick()
